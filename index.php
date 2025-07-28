8ee549b0c587483043bf9d3e1b0ea2a832301aeb
Izabella <hey.iza94@gmail.com>
2025-07-28 23:14:00 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Создание HTML - страниц</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="page1.php">404</a></li>
+      <li><a href="page2.php">Сохранине файла</a></li>
+      <li><a href="page3.php">Старт сессии</a></li>
+      <li><a href="page4.php">Старт сессии с выводом количества посещений</a></li>
+    </ul>
+  </body>
+</html>
